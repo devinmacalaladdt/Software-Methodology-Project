83e696a0e8e69c4fcd39ee221190386f17c4b679
Devin Macalalad <devinmacalaladdt@gmail.com>
2020-10-22 10:49:34 -0400

    Deposit/Withdrawal layout

--- Assignment3/src/application/Main.java
+++ Assignment3/src/application/Main.java
@@ -12,9 +12,10 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,700,320);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Transaction Manager");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
