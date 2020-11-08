c289bc0c299f0a2ac7c80cf377f6c8761ad61557
David Gasperini <dlg4242@live.com>
2020-11-08 01:34:59 -0500

    OrderLine GUI Only

--- Assignment4/src/application/Main.java
+++ Assignment4/src/application/Main.java
@@ -3,10 +3,11 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.SplitPane;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
-
+/* UNCOMMENT THIS TO RUN SANDWICH
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
@@ -15,6 +16,26 @@ public class Main extends Application {
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}*/
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			SplitPane root = (SplitPane)FXMLLoader.load(getClass().getResource("Orderline.fxml"));
+			Scene scene = new Scene(root,800,600);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Order Line");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
--- Assignment4/src/application/OrderLineController.java
+++ Assignment4/src/application/OrderLineController.java
@@ -1,5 +1,65 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+/**
+ * Controller Class to link functionality to buttons/input on GUI for orderline
+ * Contains action methods and helper methods for each input source
+ * @author Devin Macalalad, David Gasperini
+ */
 public class OrderLineController {
+	@FXML
+	private Button btnClear;
+	@FXML
+	private Button btnCE;
+	@FXML
+	private Button btnClose;
+	@FXML
+	private Button btnExport;
+	@FXML
+	private Button btnDup;
+	@FXML
+	private TextArea txtTotal;
+	@FXML
+	private TableColumn tblcolQuantity;
+	@FXML
+	private TableColumn tblcolSandwich;
+	@FXML
+	private TableColumn tblcolExtras;
+	@FXML
+	private TableColumn tblcolTotal;
 	
+	@FXML
+	private void btnClose_action() {
+		Stage stage = (Stage) btnClose.getScene().getWindow();
+	    stage.close();
+	}
+	
+	@FXML
+	private void btnClear_action() {
+		
+	}
+	
+	@FXML
+	private void btnCE_action() {
+		
+	}
+	
+	@FXML
+	private void btnExport_action() {
+		
+	}
+	
+	@FXML
+	private void btnDup_action() {
+		
+	}
+	
+	private void addEntry() {
+		
+	}
 }
